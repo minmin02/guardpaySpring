3e95941d48d7749a3d57347fef97b8fdfa56df86
minmin02 <aktr378@gmail.com>
2026-01-21 12:08:29 +0900

    Fix : yml

--- src/main/java/com/example/guardpay/global/config/AsyncConfig.java
+++ src/main/java/com/example/guardpay/global/config/AsyncConfig.java
@@ -16,7 +16,7 @@ public class AsyncConfig {
     public TaskExecutor taskExecutor() {
 
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(5);   // 기본 스레드 수
+        executor.setCorePoolSize(5);
         executor.setMaxPoolSize(10);  // 최대 스레드 수
         executor.setQueueCapacity(25); // 대기 큐 크기
         executor.setThreadNamePrefix("async-worker-");
